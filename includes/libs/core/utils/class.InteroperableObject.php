e7b40cb5ced70e0584eea0e2c3c9712294b08f86
Arnaud NICOLAS <arnaud.nicolas@vidal.fr>
2019-10-08 09:36:03 +0200

    Refactoring InteroperableObject

--- includes/libs/core/utils/class.InteroperableObject.php
+++ includes/libs/core/utils/class.InteroperableObject.php
@@ -6,6 +6,12 @@ namespace core\utils
     use core\application\Configuration;
     use core\data\SimpleXML;
     use core\tools\docs\PHPDocHelpers;
+    use ReflectionClass;
+    use ReflectionProperty;
+    use ReflectionException;
+    use Exception;
+    use DateTime;
+    use SimpleXMLElement;
 
     class InteroperableObject
     {
@@ -17,23 +23,25 @@ namespace core\utils
 
             $definition = IObjectDictionary::getDefinition(get_called_class());
 
-            $classComments = $definition["classComments"];
             $allProps = $definition["props"];
+            $format = $definition["format"];
 
             $pathThis = false;
 
             if(is_null($pRaw)){
 
-                $api = PHPDocHelpers::extractDocVar('api', $classComments);
+                $api = $definition["api"];
                 if(!$api){
                     return null;
                 }
 
-                foreach($pParams as $name=>$val){
-                    $api = str_replace('{'.$name.'}', $val, $api);
+                if(is_array($pParams)){
+                    foreach($pParams as $name=>$val){
+                        $api = str_replace('{'.$name.'}', $val, $api);
+                    }
                 }
 
-                preg_match_all('/\{([a-z\.0-9]+)\}/', $api, $matches);
+                preg_match_all('/{([a-z.0-9]+)}/', $api, $matches);
 
                 foreach($matches[1] as $match){
                     $api = str_replace("{".$match."}", Configuration::extra($match), $api);
@@ -47,52 +55,23 @@ namespace core\utils
                     }
                     $api .= $pQueryString;
                 }
-
-                $format = PHPDocHelpers::extractDocVar('format', $classComments);
-
-                if($format===false){
-                    $format = "json";
-                }
+                $pathThis = $definition["this"];
 
                 $pRaw = RestHelper::request($api, RestHelper::HTTP_GET, array(), $format);
-                $pathThis = PHPDocHelpers::extractDocVar('this', $classComments);
             }
 
             $raw = $pRaw;
 
-            switch(true){
-                case $pRaw instanceOf \SimpleXMLElement:
-                    $extracts = array(
-                        "attribute"=>function($pVal, \SimpleXMLElement $pXml){
-                            $attr = $pXml->attributes();
-                            return strval($attr[$pVal]);
-                        },
-                        "nodeValue"=>function($pVal, $pXml){
-                            if($pVal !== "1"){
-                                return null;
-                            }
-                            return strval($pXml);
-                        },
-                        "path"=>function($pVal, \SimpleXMLElement $pXml){
-                            SimpleXML::registerNameSpaces($pXml);
-                            return $pXml->xpath($pVal);
-                        }
-                    );
+            switch($format){
+                case "xml":
                     if($pathThis !== false){
                         $pRaw = $pRaw->xpath($pathThis)[0];
                     }
-                    $format = "xml";
                     break;
-                case is_array($pRaw):
-                    $extracts = array(
-                        "path"=>function($pVal, $pRaw){
-                            return Stack::get($pVal, $pRaw);
-                        }
-                    );
+                case "json":
                     if($pathThis !== false){
                         $pRaw = Stack::get($pathThis, $pRaw);
                     }
-                    $format = "json";
                     break;
                 default:
                     trigger_error('Unknown source type for parsing', E_USER_WARNING);
@@ -100,37 +79,24 @@ namespace core\utils
                     break;
             }
 
-            $extraProps = [];
             for($i = 0, $max = count($allProps); $i<$max;$i++) {
-                /** @var \ReflectionProperty $prop */
                 $prop = $allProps[$i];
-                $doc = $prop->getDocComment();
-                $name = $prop->getName();
-                foreach($extracts as $n=>$pCallback){
-                    if($n === "path"){
-                        $type = PHPDocHelpers::extractDocVar('var', $doc);
-                        $path = PHPDocHelpers::extractDocVar('path', $doc);
-                        if($path === false){
-                            $extraProps[$name] = array("type"=>$type);
-                            continue;
-                        }
-                        $this->setPropFromData($name, $type, $pCallback($path, $pRaw), $format);
-                        break;//To check
+                $name = $prop["name"];
+                $type = $prop["type"];
+                $path = $prop["path"];
+                $callback = $prop["callback"];
+                if(!is_null($callback)){
+                    if($prop["setProp"]){
+                        $this->setPropFromData($name, $type, $callback($path, $pRaw), $format);
+                    }else{
+                        $this->{$name} = self::extractValue($type, $callback($path, $pRaw));
                     }
-                    $val = PHPDocHelpers::extractDocVar($n, $doc);
-                    if($val !== false){
-                        $this->{$name} = $pCallback($val, $pRaw);
-                        break;//To check
+                }else{
+                    if(!$path){
+                        continue;
                     }
+                    $this->setPropFromData($name, $type, $definition['defaultExtract']($path, $raw));
                 }
-            }
-
-            foreach($extraProps as $name=>$prop){
-                $stackId = PHPDocHelpers::extractDocVar($name, $classComments);
-                if(!$stackId){
-                    continue;
-                }
-                $this->setPropFromData($name, $prop["type"], $extracts['path']($stackId, $raw));
             }
 
             return $pRaw;
@@ -167,7 +133,7 @@ namespace core\utils
         /**
          * @param string $pType
          * @param mixed $pSource
-         * @return array|null
+         * @return mixed
          */
         static public function extractValue($pType, $pSource){
             switch($pType){
@@ -175,7 +141,13 @@ namespace core\utils
                     $val = strval($pSource);
                     break;
                 case '\DateTime':
-                    $val = new \DateTime(strval($pSource));
+                    try{
+                        $val = new DateTime(strval($pSource));
+                    }
+                    catch(Exception $e){
+                        trigger_error("An error occurred while initializing DateTime instance ".$e->getMessage(), E_USER_WARNING);
+                        return null;
+                    }
                     break;
                 case 'bool':
                     $val = strval($pSource) === 'true';
@@ -205,10 +177,92 @@ namespace core\utils
                 return self::$classes[$pClassName];
             }
 
-            $reflec = new \ReflectionClass($pClassName);
+            try{
+                $reflec = new ReflectionClass($pClassName);
+            }
+            catch(ReflectionException $e){
+                trigger_error("An error occurred while initializing RelfectionClass ".$e->getMessage(), E_USER_WARNING);
+                return null;
+            }
+            $classComments = $reflec->getDocComment();
+            $format = PHPDocHelpers::extractDocVar("format", $classComments);
+            if(!$format){
+                $format = "json";
+            }
+
+            $props = [];
+
+            $p = $reflec->getProperties(ReflectionProperty::IS_PUBLIC|ReflectionProperty::IS_PROTECTED|ReflectionProperty::IS_PRIVATE);
+
+            switch($format){
+                case "xml":
+                    $extracts = array(
+                        "attribute"=>function($pVal, SimpleXMLElement $pXml){
+                            $attr = $pXml->attributes();
+                            return strval($attr[$pVal]);
+                        },
+                        "nodeValue"=>function($pVal, $pXml){
+                            if($pVal !== "1"){
+                                return null;
+                            }
+                            return strval($pXml);
+                        },
+                        "path"=>function($pVal, SimpleXMLElement $pXml){
+                            SimpleXML::registerNameSpaces($pXml);
+                            return $pXml->xpath($pVal);
+                        }
+                    );
+                    break;
+                default:
+                case "json":
+                    $extracts = array(
+                        "path"=>function($pVal, $pRaw){
+                            return Stack::get($pVal, $pRaw);
+                        }
+                    );
+                    break;
+            }
+
+            foreach($p as &$property){
+                $comment = $property->getDocComment();
+                $prop = array(
+                    "name"=>$property->getName(),
+                    "type"=>PHPDocHelpers::extractDocVar('var', $comment),
+                    "path"=>PHPDocHelpers::extractDocVar('path', $comment),
+                    "callback"=>null,
+                    "setProp"=>false
+                );
+
+                foreach($extracts as $n=>$pCallback){
+                    if($n === "path"){
+                        if($prop["path"] === false){
+                            break;
+                        }
+                        $prop["setProp"] = true;
+                        $prop["callback"] = $pCallback;
+                        break;
+                    }
+                    $val = PHPDocHelpers::extractDocVar($n, $comment);
+                    if($val !== false){
+                        $prop["callback"] = $pCallback;
+                        $prop["path"] = $val;
+                        break;
+                    }
+                }
+
+                if(is_null($prop["callback"])){
+                    $prop["path"] = PHPDocHelpers::extractDocVar($prop["name"], $classComments);
+                }
+
+                $props[] = $prop;
+            }
+
             self::$classes[$pClassName] = array(
-                "classComments"=>$reflec->getDocComment(),
-                "props"=>$reflec->getProperties(\ReflectionProperty::IS_PUBLIC|\ReflectionProperty::IS_PROTECTED|\ReflectionProperty::IS_PRIVATE)
+                "api"=>PHPDocHelpers::extractDocVar('api', $classComments),
+                "this"=>PHPDocHelpers::extractDocVar('this', $classComments),
+                "format"=>$format,
+                "props"=>$props,
+                "defaultExtract"=>$extracts["path"]
             );
             return self::$classes[$pClassName];
         }

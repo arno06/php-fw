a4163e87ba7031d0ed7efdcbaac8df0bde03aa75
anicolas <arno06@gmail.com>
2023-11-30 14:26:44 +0100

    PHP 8.2

--- includes/applications/main/modules/front/controllers/controller.index.php
+++ includes/applications/main/modules/front/controllers/controller.index.php
@@ -2,8 +2,6 @@
 namespace app\main\controllers\front
 {
     use core\application\DefaultController;
-    use core\db\Query;
-    use core\tools\debugger\Debugger;
 
     class index extends DefaultController
     {
